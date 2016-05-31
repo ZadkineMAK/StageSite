22ee33a5030692525b2f56be0e75965d9a8b0648
armandojanssen <SHushilla1993>
2016-05-31 09:02:36 +0200

    testinge

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /**
+         * test
+         */
 //         $this->call(CompaniesTableSeeder::class);
 //         $this->call(RolesTableSeeder::class);
     }
